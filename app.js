c42ef217753f8ad4562483080555956d1aa8d587
heshamaref388 <heshamaref388@gmail.com>
2024-12-22 20:05:09 +0200

    Initial commit of the project files

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-// app.js
 import express, { json } from "express";
 import cors from "cors";
-import { connect } from "mongoose";
+import mongoose from "mongoose"; // Import the database connection
 import exchangeRoutes from "./routes/exchangeRoutes.js"; // Import the routes
 import metadataRoutes from "./routes/metadataRoutes.js"; // Import the routes
 import candleRoutes from "./routes/candleRoutes.js"; // Import the routes
@@ -10,12 +9,15 @@ const app = express();
 app.use(json());
 app.use(cors());
 
+const uri =
+  // "mongodb+srv://heshamaref38:Hesam2020@cluster0.ci9ce.mongodb.net/Mydatabase?retryWrites=true&w=majority"; // Updated connection string
+  // "mongodb://localhost:27017/mydatabase"; // Updated connection string
+  "mongodb+srv://heshamaref:hesham2020@cluster0.g2vdp.mongodb.net/Mydatabase"; // Updated connection string
+
 // Connect to MongoDB
-connect("mongodb://localhost:27017/mydatabase", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log("MongoDB connected"))
+mongoose
+  .connect(uri)
+  .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
 // Use the exchange routes
